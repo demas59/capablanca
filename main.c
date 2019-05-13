91c2be93d0cd97e32e906cf8cf700031cd0de437
demas59 <demas59@github.com>
2019-05-13 14:42:28 +0200

    affichage plateau

--- main.c
+++ main.c
@@ -4,6 +4,46 @@
 #include "struct.h"
 
 
+void setColor(int i,int j,Piece * pieces){
+	if(i == 0 || i == 1){
+				(*(pieces+(i*10+j)))->color=0;
+			}else{
+				if(i == 6 || i == 7){
+					(*(pieces+(i*10+j)))->color=1;
+				}else{
+					(*(pieces+(i*10+j)))->color=-1;
+				}
+	}
+}
+
+void setType(int i,int j, Piece * pieces){
+	if((j == 0 || j == 9) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 't';
+	}
+	if((j == 1 || j == 8) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 'c';
+	}
+	if((j == 2) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 'x';
+	}
+	if((j == 3 || j == 6) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 'f';
+	}
+	if((j == 4) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 'q';
+	}
+	if((j == 5) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 'k';
+	}
+	if((j == 7) && (i == 0 || i == 7)){
+		(*(pieces+(i*10+j))) -> type = 'i';
+	}
+	if(i == 1 || i == 6){
+		(*(pieces+(i*10+j))) -> type = 'p';
+	}
+}
+
+
 Piece newPion(char type,int equipe){
 	Piece pion = (Piece)malloc(sizeof(struct piece_));
 	pion->type = type;
@@ -24,6 +64,8 @@ Grille initialisation(){
 	for(i=0;i<8;i++){
 		for(j=0;j<10;j++){
 			(*(pieces+(i*10+j))) = newPion('0', 0);
+			setColor(i,j,pieces);
+			setType(i,j,pieces);
 		}
 	}
 	Grille grille = (Grille) malloc(sizeof(struct grille_));
@@ -31,16 +73,23 @@ Grille initialisation(){
 	return grille;
 }
 
+
 /*Affiche la grille de jeu*/
 void affichage(Grille grille){
+	system ("clear");
+	printf("\n\t|-1---2---3---4---5---6---7---8---9--10-|\n\t");
+	printf("|---------------------------------------|\n\t");
 	int i,j;
 	Piece * plateau = grille -> pions;
 	for(i=0;i<8;i++){
 		for(j=0;j<10;j++){
-			printf("%c",(*(plateau+(i*10+j)))->type);
+			printf("| %c ",(*(plateau+(i*10+j)))->type);
+			if(j == 9){
+				printf("|%d\n\t|---------------------------------------|\n\t", i);
+			}
 		}
-		printf("\n");
 	}
+	printf("|-1---2---3---4---5---6---7---8---9--10-|\n\t");
 }
 
 void mouvement(Grille grille, Piece piece){
