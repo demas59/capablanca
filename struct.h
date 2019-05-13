9003397150e124b8ec847109b886057feb22b9db
demas59 <demas59@github.com>
2019-05-13 08:20:44 +0200

    Structure projet

---
+++ struct.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct coord_ {
+	int x;
+	int y;
+} * Coord;
+
+typedef struct piece_ {
+	int color;
+	Coord coord;
+	int actif;
+	int type;
+} * Piece;
+
+typedef struct grille_ {
+	Piece * pions;
+	Piece mort;
+} * Grille;
+
