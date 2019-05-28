27144b05256725aa4e655fcb1ee2c28033f11868
Marchand <1ttoommyy@gmail.com>
2019-05-28 16:16:49 +0200

    clear code

--- main.c
+++ main.c
@@ -7,16 +7,6 @@ int main(void){
 	Grille grille = initialisation();
 	int echec_et_mat = 0;
 	Piece piece;
-
-	Piece pion = createPiece(2,2,0,'p');
-	Piece pion2 = createPiece(1,5,0,'p');
-	Piece cavalier = createPiece(1,4,4,'c');
-	Piece fou = createPiece(1,4,5,'f');
-
-	// placerPiece(grille,pion);
-	// placerPiece(grille,pion2);
-	// placerPiece(grille,cavalier);
-	// placerPiece(grille,fou);
 	affichage(grille);
 
 	clearDeplacement(grille);
