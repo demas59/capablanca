d115d97ae7441a0905e07b8186fc5d0323b5fcac
demas59 <demasay@gmail.com>
2019-06-05 15:04:29 +0200

    Correction des main/makefile

--- Chess/main.c
+++ Chess/main.c
@@ -20,6 +20,15 @@ void pause()
     }
 }
 
+int checkVoid(Coord coordClick, Grille g){
+            if (g -> pions[getIndice(coordClick -> x, coordClick -> y)] -> color == 0){
+                return 0;
+            }
+            else{
+                return 1;
+        }
+}
+
 int main(int argc, char *argv[])
 	{
     SDL_Surface *ecran = NULL, *imageDeFond = NULL;
@@ -31,12 +40,12 @@ int main(int argc, char *argv[])
     imageDeFond = IMG_Load("echiquier.png");
 
     SDL_Init(SDL_INIT_VIDEO);
-    /* Création de la fenetre de base */
+    /* Crï¿½ation de la fenetre de base */
     ecran = SDL_SetVideoMode(680, 560, 32, SDL_HWSURFACE | SDL_RESIZABLE | SDL_DOUBLEBUF);
     SDL_WM_SetCaption("Chess Projet", NULL);
 
     SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
-    // Initialisation des positions de départ de pieces Noires
+    // Initialisation des positions de dï¿½part de pieces Noires
     Grille g1 = initialisation();
     int m ; int l;
     for (m = 0; m < 8 ; m++){
@@ -119,8 +128,37 @@ int main(int argc, char *argv[])
     }
 
     SDL_Flip(ecran);
-    pause();
+//    pause();
 
-    SDL_FreeSurface(imageDeFond); /* On libère la surface */
+//
+SDL_Event event;
+Coord coordClick;
+int continuer = 1;
+while(continuer){
+    SDL_WaitEvent(&event);
+    switch(event.type)
+{
+    case SDL_QUIT:
+        continuer = 0;
+        break;
+    case SDL_MOUSEBUTTONUP:
+        if (event.button.button == SDL_BUTTON_LEFT){ /* Si on fait un clique gauche, on lance l'instruction*/
+            coordClick = createCoord(event.button.x/68,event.button.y/70);
+            if (!checkVoid(coordClick, g1)){
+                proposerDeplacer(g1, g1 -> pions[getIndice(coordClick -> x, coordClick -> y)]);
+
+            }
+
+ //           continuer = 0;
+        }
+        break;
+}
+}
+
+//
+
+    SDL_FreeSurface(imageDeFond); /* On libï¿½re la surface */
     SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
